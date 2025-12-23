edbe5d7958b6d66db069d6a4374efdd2d16c05e0
Isaias Castillo <isaias.castillo@utp.ac.pa>
2025-12-23 10:19:45 -0500

    feat: add initial implementation of ApartmentRow type and necessary imports in apartments page

---
+++ src/app/ph/[phId]/apartments/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { supabase } from "@/lib/supabase/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useParams, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { es } from "zod/v4/locales";
+
+type ApartmentRow = {
+    ph_id: string;
+    flor_id: string;
+    code: string;
+    order_index: number;
+};
